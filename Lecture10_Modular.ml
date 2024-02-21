2f7637588c27372e3a7a39789c82c2d047fe6b83
raymond <raymond.lin.05@gmail.com>
2024-02-21 17:09:09 -0500

    more modular stuff

--- Lecture10_Modular.ml
+++ Lecture10_Modular.ml
@@ -35,10 +35,11 @@ let empty_list = MyList.Nil
 (* stack *)
 (* could also use the OCaml list library, linked list is perfect for stack *)
 module MyStack = struct
-  type 'a stack =
+  type 'a t =
     | Empty
-    | Entry of 'a * 'a stack
+    | Entry of 'a * 'a t
 
+  let empty = Empty
   let push x s = Entry (x, s)
 
   exception EmptyStack
@@ -51,6 +52,32 @@ module MyStack = struct
   let pop = function
     | Empty -> raise EmptyStack
     | Entry (_, s) -> s
+
+  let is_empty = function
+    | Empty -> true
+    | _ -> false
+end
+
+module ListStack = struct
+  type 'a t = 'a list
+
+  let empty = []
+
+  exception EmptyStack
+
+  let push x s = x :: s
+
+  let pop = function
+    | [] -> raise EmptyStack
+    | _ :: s -> s
+
+  let peek = function
+    | [] -> raise EmptyStack
+    | x :: _ -> x
+
+  let is_empty = function
+    | [] -> true
+    | _ -> false
 end
 
 (* Functional data structures are persistent (operations do not mutate
@@ -72,3 +99,25 @@ end
 (* this doesn't work, because signatures encapsulate methods that aren't defined
    in the signature *)
 (** [let b = MyMath.other_function 5] *)
+
+module type Stack = sig
+  type 'a t
+  (* abstract data type, can be list or your own stack *)
+  (* basically a generic type, could also define a concrete type here *)
+
+  val empty : 'a t
+  val push : 'a -> 'a t -> 'a t
+  val pop : 'a t -> 'a t
+  val peek : 'a t -> 'a
+  val is_empty : 'a t -> bool
+end
+
+(* these are sealed because the internal implementation is no longer
+   accessible *)
+(* that is why empty is necessary, this is encapsulation *)
+module ListStackSealed : Stack = ListStack
+
+module MyStackSealed : Stack = MyStack
+(** using t for abstract types is convention, write [MyStackSealed.t] *)
+
+(* write documentation functions in signature only, not implementation *)
